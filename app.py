25409dd908fa2ca22a2a36efd8252db642009175
AlbertQuarshie <162796723+AlbertQuarshie@users.noreply.github.com>
2026-05-01 13:58:53 +0300

    Implementing the number of choices per difficulty

--- app.py
+++ app.py
@@ -5,4 +5,17 @@ def choose_difficulty():
     print("1: Easy")
     print("2: Moderate")
     print("3: Hard")
-    
+
+
+    while True:
+        choice = input("Select Choice (1,2,3):")
+    if choice == '1':
+        return 10
+    elif choice == '2':
+        return 5
+    elif choice == '3':
+        return 3
+    else:
+        print("Invalid choice. Select 1, 2 or 3")
+
+        
